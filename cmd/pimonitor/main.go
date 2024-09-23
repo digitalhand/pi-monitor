aba936b3ea02b0316e6d55b9d0a99892ac714bd8
Jesse Hernandez <futcomker@gmail.com>
2024-09-23 14:19:28 -0700

    feat: update runners and fix bug

--- cmd/pimonitor/main.go
+++ cmd/pimonitor/main.go
@@ -6,25 +6,26 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func VerifyPathExists(path string) (string, error) {
-    entries, err := os.ReadDir(path)
-		if err != nil {
-			log.Fatal(err)
-		}
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-		for _, e := range entries {
-			return e.Name(), nil
- 		}
-	return "error", nil 
+	for _, e := range entries {
+		return e.Name(), nil
+	}
+	return "error", nil
 }
 
 func main() {
 	path := "/sys/bus/w1/devices/"
 	ds18b20, err := VerifyPathExists(path)
 	if err != nil {
-		log.Fatal(err) 
+		log.Fatal(err)
 	}
 
 	for true {
@@ -32,26 +33,33 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println("Temp: ", faren)	
-  }
+		fmt.Println("Temp: ", faren)
+		//time.Sleep(3 * time.Second)
+	}
 
 }
 
-func GetSensorTemperature(fileName string)(float64, float64, error) {
+func GetSensorTemperature(fileName string) (float64, float64, error) {
 	fileData, err := os.ReadFile(fileName)
 	if err != nil {
 		log.Fatal(err)
 	}
 	breakLine := "\n"
+	var farenheit float64 = 0.0
+	var celcius float64 = 0.0
 
-	secondLine := strings.Split(string(fileData[:]), breakLine)[1]
-	//fmt.Print("SecondLine: ", secondLine)
-	temperatureData := strings.Split(secondLine, " ")[9]
-	//fmt.Print("Temperature Data", temperatureData)
-	temperature, _ := strconv.ParseFloat(temperatureData[2:], 64)
-	celcius := (temperature / 1000)
-	farenheit := (celcius * 1.8) + 32
+	if len(fileData) != 0 {
+		secondLine := strings.Split(string(fileData[:]), breakLine)[1]
+		temperatureData := strings.Split(secondLine, " ")[9]
+		temperature, _ := strconv.ParseFloat(temperatureData[2:], 64)
+		celcius := (temperature / 1000)
+		farenheit := (celcius * 1.8) + 32
+		return celcius, farenheit, err
 
-	return celcius, farenheit, err
+	} else {
+		fmt.Println("No Temp To Report, taking a small break...")
+		time.Sleep(5 * time.Second)
+		return celcius, farenheit, err
+	}
 
 }
